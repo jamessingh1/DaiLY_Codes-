1756fa3efd59e12870d2ae99ad03425f37bdd871
AnkitSingh <rajput645ankit@gmail.com>
2025-09-05 00:03:09 +0530

    init commit

---
+++ src/string.java
@@ -0,0 +1,13 @@
+
+public class string {
+    public static void main(String[] args) {
+        String a1 = "aaa";
+        String b2 = "bbb";
+        String c3 = "ccc";
+
+
+        System.out.println("a1 has " + a1.length()+ " characters");
+        System.out.println("b2 has " + b2.length() + " characters");
+        System.out.println("c3 has " + c3.length() + " characters");
+    }
+}
