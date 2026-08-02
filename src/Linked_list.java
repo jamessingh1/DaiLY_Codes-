ea5db936fb24341ee41fc30dfe70fb4281de6bcc
AnkitSingh <rajput645ankit@gmail.com>
2026-08-02 21:14:59 +0530

    commit!

---
+++ src/Linked_list.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class Linked_list {
+    public static void main(String[] args) {
+
+        LinkedList nums = new LinkedList();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(2);
+        nums.addFirst(3);
+
+        System.out.println(nums);
+    }
+}
