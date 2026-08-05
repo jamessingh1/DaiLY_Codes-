900f25030c6122c0ad234daf4d5268e0566d1b3d
AnkitSingh <rajput645ankit@gmail.com>
2026-08-05 13:25:18 +0530

    commit done!

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,7 +1,39 @@
 class node{
-    
-}
-public class LinkedList {
-    public void add(int i) {
+    int data;
+    node next;// (reference = next);
+
+    public node(int data){//constructor
+        this.data = data;
+        next = null;
     }
 }
+
+public class LinkedList {
+    node head = null;
+
+    public void add(int data) {
+        node newNode = new node(data);
+
+        node current = head;
+        if (head == null)
+            head = newNode;
+
+        else {
+            while (current.next != null)
+                current = current.next;
+            current.next = newNode;
+        }
+    }
+
+    public void display() {
+        node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+
+
--- src/Linked_list.java
+++ src/Linked_list.java
@@ -1,15 +1,10 @@
-
 public class Linked_list {
     public static void main(String[] args) {
-
         LinkedList nums = new LinkedList();
+        nums.add(7);
+        nums.add(8);
+        nums.add(6);
 
-        nums.add(1);
-        nums.add(2);
-        nums.add(2);
-        nums.addFirst(3);
-        nums.peek(2);
-
-        System.out.println(nums);
+        nums.display();
     }
 }
