09d49d51d9401210f281b7fc489221b9ea177741
AnkitSingh <rajput645ankit@gmail.com>
2026-08-08 23:19:35 +0530

    commit done!

--- src/tree.java
+++ src/tree.java
@@ -1,2 +1,19 @@
 public class tree {
+    public static void main(String[] args) {
+        tree t = new tree();
+        t.insert(10);
+        t.insert(59);
+        t.insert(30);
+        t.insert(45);
+
+        t.show();
+    }
+
+    public void insert(int data){
+
+    }
+
+    public void show(){
+
+    }
 }
