b796a9207b96bc1cd220e4986b27f6c08346bf76
AnkitSingh <rajput645ankit@gmail.com>
2026-08-17 22:14:47 +0530

    small leetcode problem

--- src/LeetCode_Solver/Day_01.java
+++ src/LeetCode_Solver/Day_01.java
@@ -2,6 +2,21 @@ package LeetCode_Solver;
 
 public class Day_01 {
     public static void main(String[] args) {
-        System.out.println("Day 1 Hopes great");
+        int[] nums = {5,8,7,31,10,14}; 
+        
+        System.out.println(largestElement(nums));
     }
+
+    public static int largestElement(int[] nums){
+            int largestElement = nums[0];
+
+            for(int i = 0; i < nums.length; i++){
+                if(largestElement < nums[i]){
+                    largestElement = nums[i];
+                }
+            }
+            return largestElement;
+           
+        }
+
 }
