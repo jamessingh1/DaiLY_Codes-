37279838615b1c97c1c868e771b17599f5e3845e
AnkitSingh <rajput645ankit@gmail.com>
2026-08-18 23:33:49 +0530

    Up to date

---
+++ src/Demo/Casting.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class Casting {
+
+    // Conversion & Casting
+
+    public static void main(String[] args){
+
+        byte c = 127;
+
+    int a = 789;
+    byte b = (byte) a;
+
+    System.out.println(b);
+
+  }  
+
+    
+}
---
+++ src/Demo/literals.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class literals{
+    public static void main(String[] args){
+        //int nums = 0b011; (For Binary Number)
+       // int nums = 0x7F;  (For Decimal numbers)
+       // int nums = 100_00_00;
+       //double nums = 12e101;
+       char A = 'a';
+       A++;
+        
+        System.out.println(A);
+    }
+}
