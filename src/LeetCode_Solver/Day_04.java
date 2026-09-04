7348cf1396e082fc767d0ace4e2c15862c42b052
AnkitSingh <rajput645ankit@gmail.com>
2026-09-04 10:26:07 +0530

    Sept 4th Day 1st Push

---
+++ src/LeetCode_Solver/Day_04.java
@@ -0,0 +1,15 @@
+public class Day_04 {
+
+    public static void main(String[] args){
+        int a = 7;
+        int b = 9;
+
+        float x = 8;
+        float y = 8;
+
+        boolean result = (a<b || x>y);
+        System.out.println("Gues the Output: " + result);
+
+    }
+    
+}
