2e2ac5de4616ceb011c223f31bd441e9411b0f28
AnkitSingh <rajput645ankit@gmail.com>
2026-09-08 23:45:53 +0530

    Hii!

--- src/LeetCode_Solver/hello.java
+++ src/LeetCode_Solver/hello.java
@@ -3,7 +3,29 @@ package LeetCode_Solver;
 public class hello {
 
     public static void main (String[] args){
-        int[] nums = {9,8,7,6,5,4,3,2,1};
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int k = 8;
+
+        rotateleft(nums,k);
+
+        for (int num: nums){
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotateleft(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        for(int j = 0; j < k; j++){
+            int first = nums[0];
+
+            for(int i = 0; i < nums.length - 1; i++){
+                nums[i] = nums[i+1];
+            }
+
+            nums[nums.length - 1] = first;
+        }
     }
     
 }
